242fc16a3626b1495e468b5dfbc7b2b28a38723a
afshin.amini <afshin.amini@kuehne-nagel.com>
2024-04-02 14:12:05 +0200

    EhCache is used and Cache Event Listener is added

--- src/main/java/com/example/democache/persistence/Book.java
+++ src/main/java/com/example/democache/persistence/Book.java
@@ -1,11 +1,13 @@
 package com.example.democache.persistence;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
-public class Book {
+public class Book implements Serializable {
     private long id;
     private String name;
     private String author;
